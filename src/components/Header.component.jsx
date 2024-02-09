fab9dadd652099b7b89e24396d38b34fa010e1a5
Sree Harsha <95706697+Sree-Harsha-PS@users.noreply.github.com>
2024-02-10 02:23:15 +0530

    Update Header.component.jsx

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -6,7 +6,7 @@ function Header() {
         <header>
             <h1>
                 <HighlightIcon />
-                Pravar's Keeper
+                Sree's Keeper
             </h1>
         </header>
     );
